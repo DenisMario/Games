28de630ed83fa1bb3bf4d3c82bddb80264ffc595
DenisMario <120132656+DenisMario@users.noreply.github.com>
2023-01-13 04:21:04 +0200

    Update RockPaperScissors.py

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -5,7 +5,7 @@ import random
 
 root = Tk()
 root.geometry("400x400")
-root.title("Rock Paper Scissor Game")
+root.title("Rock Paper Scissor Game by Denis Andonov")
  
 computer_value = {
     "0": "Rock",
@@ -14,8 +14,6 @@ computer_value = {
 }
  
 
- 
- 
 def reset_game():
     b1["state"] = "active"
     b2["state"] = "active"
@@ -23,16 +21,13 @@ def reset_game():
     l1.config(text="Player              ")
     l3.config(text="Computer")
     l4.config(text="")
- 
-
- 
- 
+  
 def button_disable():
     b1["state"] = "disable"
     b2["state"] = "disable"
     b3["state"] = "disable"
  
-# If player selected rock 
+# If player chooses rock 
 def isrock():
     c_v = computer_value[str(random.randint(0, 2))]
     if c_v == "Rock":
@@ -46,7 +41,7 @@ def isrock():
     l3.config(text=c_v)
     button_disable()
  
-# If player selected paper
+# If player chooses paper
 def ispaper():
     c_v = computer_value[str(random.randint(0, 2))]
     if c_v == "Paper":
@@ -60,7 +55,7 @@ def ispaper():
     l3.config(text=c_v)
     button_disable()
  
-# If player selected scissor 
+# If player chooses scissors
 def isscissor():
     c_v = computer_value[str(random.randint(0, 2))]
     if c_v == "Rock":
@@ -73,24 +68,16 @@ def isscissor():
     l1.config(text="Scissor         ")
     l3.config(text=c_v)
     button_disable()
- 
- 
-# Add Labels, Frames and Button
-Label(root,
-      text="Rock Paper Scissor by Denis",
-      font="normal 20 bold",
-      fg="blue").pack(pady=20)
+  
+#Labels, Frames and Button
+Label(root,text="Rock Paper Scissor by Denis",font="normal 20 bold",fg="blue").pack(pady=20)
  
 frame = Frame(root)
 frame.pack()
  
-l1 = Label(frame,
-           text="Player              ",
-           font=10)
+l1 = Label(frame,text="Player              ",font=10)
  
-l2 = Label(frame,
-           text="VS             ",
-           font="normal 10 bold")
+l2 = Label(frame,text="VS             ",font="normal 10 bold")
  
 l3 = Label(frame, text="Computer", font=10)
  
@@ -98,37 +85,22 @@ l1.pack(side=LEFT)
 l2.pack(side=LEFT)
 l3.pack()
  
-l4 = Label(root,
-           text="",
-           font="normal 20 bold",
-           bg="white",
-           width=15,
-           borderwidth=2,
-           relief="solid")
+l4 = Label(root,text="",font="normal 20 bold",bg="white",width=15,borderwidth=2,relief="solid")
 l4.pack(pady=20)
  
 frame1 = Frame(root)
 frame1.pack()
  
-b1 = Button(frame1, text="Rock",
-            font=10, width=7,
-            command=isrock)
+b1 = Button(frame1, text="Rock",font=10, width=7,command=isrock)
  
-b2 = Button(frame1, text="Paper ",
-            font=10, width=7,
-            command=ispaper)
+b2 = Button(frame1, text="Paper ",font=10, width=7,command=ispaper)
  
-b3 = Button(frame1, text="Scissor",
-            font=10, width=7,
-            command=isscissor)
+b3 = Button(frame1, text="Scissors",font=10, width=7,command=isscissor)
  
 b1.pack(side=LEFT, padx=10)
 b2.pack(side=LEFT, padx=10)
 b3.pack(padx=10)
  
-Button(root, text="Reset Game",
-       font=10, fg="red",
-       bg="white", command=reset_game).pack(pady=20)
+Button(root, text="Reset Game",font=10, fg="red",bg="white", command=reset_game).pack(pady=20)
  
-# Execute Tkinter
 root.mainloop()
